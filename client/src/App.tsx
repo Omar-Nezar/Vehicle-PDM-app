2f4befa69aef0c48afddaf678c850ad4485256d7
Omar Nezar <frcollege2@gmail.com>
2026-07-14 18:12:17 +0400

    Added imports and routes for login & forgotpwd

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,16 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 
 import Login from './pages/car_owner/Login'
 import Register from './pages/car_owner/Register'
-
+import ElevatedLogin from './pages/elevated/ElevatedLogin'
+import ForgotPassword from './pages/common/ForgotPassword'
 function App() {
 
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/elevatedlogin" element={<ElevatedLogin />} />
+      <Route path="/forgotpassword" element={<ForgotPassword />} />
       <Route path="/register" element={<Register />} />
     </Routes>
   )
