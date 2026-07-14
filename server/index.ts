3af4468dfa5b7636ddeadd189d3e6e5fee7df835
Omar Nezar <frcollege2@gmail.com>
2026-07-14 18:15:57 +0400

    Added intital server configuration

---
+++ server/index.ts
@@ -0,0 +1,27 @@
+import express, { type Request, type Response } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("API is running...");
+});
+
+const PORT = process.env.PORT || 5000;
+
+try {
+    mongoose.connect(process.env.MONGO_URI as string)
+        .then(() => {
+            console.log("MongoDB connected");
+            app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+        })
+} catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+}
