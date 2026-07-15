35e700455801b25bfe03d75828ba61e7ff629f4c
Omar Nezar <frcollege2@gmail.com>
2026-07-15 19:04:50 +0400

    Added sonner container in app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,23 +1,25 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
+import { Toaster } from "@/components/ui/sonner"
 
-import Login from './pages/car_owner/Login'
+import Login from './pages/common/Login'
 import Register from './pages/car_owner/Register'
-import ElevatedLogin from './pages/elevated/ElevatedLogin'
 import ForgotPassword from './pages/common/ForgotPassword'
 import CarOwnerHome from './pages/car_owner/carOwnerHome'
 
 function App() {
 
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/login" />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/elevatedlogin" element={<ElevatedLogin />} />
-      <Route path="/forgotpassword" element={<ForgotPassword />} />
-      <Route path="/register" element={<Register />} />
+    <>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+        <Route path="/register" element={<Register />} />
 
-      <Route path="/carownerhome" element={<CarOwnerHome />} />
-    </Routes>
+        <Route path="/carownerhome" element={<CarOwnerHome />} />
+      </Routes>
+      <Toaster richColors position="top-right" />
+    </>
   )
 }
 
