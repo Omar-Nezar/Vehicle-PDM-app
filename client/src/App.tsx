7996ab0b54497036c7b7fa54218ec81311ecf13e
Omar Nezar <frcollege2@gmail.com>
2026-07-15 12:47:02 +0400

    Added car owner home route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,8 @@ import Login from './pages/car_owner/Login'
 import Register from './pages/car_owner/Register'
 import ElevatedLogin from './pages/elevated/ElevatedLogin'
 import ForgotPassword from './pages/common/ForgotPassword'
+import CarOwnerHome from './pages/car_owner/carOwnerHome'
+
 function App() {
 
   return (
@@ -13,6 +15,8 @@ function App() {
       <Route path="/elevatedlogin" element={<ElevatedLogin />} />
       <Route path="/forgotpassword" element={<ForgotPassword />} />
       <Route path="/register" element={<Register />} />
+
+      <Route path="/carownerhome" element={<CarOwnerHome />} />
     </Routes>
   )
 }
