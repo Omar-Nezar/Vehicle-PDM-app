ce428b208d4e4bdadbb2eae80214c9539ee24c09
Omar Nezar <frcollege2@gmail.com>
2026-07-15 19:04:26 +0400

    Added alias for schemas file path

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,7 +10,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./@")
+      "@": path.resolve(__dirname, "./@"),
+      "@schemas": path.resolve(__dirname, "../shared/schemas")
     }
   }
 })
