75758c915bf0a692861b59bce3933ef4ca913a51
Omar Nezar <frcollege2@gmail.com>
2026-07-15 12:51:03 +0400

    Added name to registration and added login

--- server/controllers/auth.controller.ts
+++ server/controllers/auth.controller.ts
@@ -1,14 +1,17 @@
 import { type Request, type Response } from "express";
-import userModel from "../models/userModel.js";
 
+import userModel from "../models/userModel.js";
+import { comparePassword } from "../utils/hash.js"
+
+// Register
 export const registerUser = async (req: Request, res: Response) => {
   try {
-    const { email, password, role } = req.body;
+    const { name, email, password, role } = req.body;
 
     // 1. Basic validation
-    if (!email || !password) {
+    if (!name || !email || !password) {
       return res.status(400).json({
-        message: "Email and password are required",
+        message: "Name, email and password are required",
       });
     }
 
@@ -23,6 +26,7 @@ export const registerUser = async (req: Request, res: Response) => {
 
     // 4. Create user
     const user = await userModel.create({
+      name,
       email,
       password: password,
       role, // optional (defaults if not provided)
@@ -32,8 +36,9 @@ export const registerUser = async (req: Request, res: Response) => {
     const userObj = user.toObject();
     const { password: _password, ...userWithoutPassword } = userObj;
 
+    // 6. Return success response
     return res.status(201).json({
-      message: "User registered successfully",
+      message: "Registration successful",
       user: userWithoutPassword,
     });
   } catch (error) {
@@ -44,3 +49,48 @@ export const registerUser = async (req: Request, res: Response) => {
     });
   }
 };
+
+// Login
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    // 1. Basic validation
+    if (!email || !password) {
+      return res.status(400).json({
+        message: "Email and password are required",
+      });
+    }
+    // 2. Check if user exists
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    // 3. Check if password is correct
+    const isMatch = await comparePassword(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({
+        message: "Invalid credentials",
+      });
+    }
+
+    // 5. Remove password from response
+    const userObj = user.toObject();
+    const { password: _password, ...userWithoutPassword } = userObj;
+
+    // 6. Return success response
+    return res.status(200).json({
+      message: "Login successful",
+      user: userWithoutPassword,
+    });
+  } catch (error) {
+    console.error(error);
+
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
+}
