302425146e5278c993770afd1ad457a6019cc4b7
Omar Nezar <frcollege2@gmail.com>
2026-07-22 12:17:07 +0400

    Updated to adapt based on user type

--- client/src/pages/common/SideBar.tsx
+++ client/src/pages/common/SideBar.tsx
@@ -9,54 +9,95 @@ import {
     SidebarMenuButton,
     SidebarRail,
 } from "@/components/ui/sidebar";
-import { Home, Users } from "lucide-react";
+import { Home, Users, CarFront } from "lucide-react";
 import { NavLink } from "react-router-dom";
+import { cn } from "@/lib/utils";
+
+import getUserType from "src/functions/utility/getUserType";
 
 export default function AppSidebar() {
+
+    const linkActive = (isActive: boolean) =>
+        cn(
+            "flex items-center gap-2 py-3 w-full h-full rounded-2xl",
+            isActive
+                ? "bg-accent text-accent-foreground"
+                : "text-muted-foreground"
+        );
+
+    const type = getUserType()
+
     return (
         <Sidebar collapsible="icon">
             <SidebarContent>
-                <SidebarGroup>
-                    <SidebarGroupLabel>Admin</SidebarGroupLabel>
+                {type === "car_owner" &&
+                    <>
+                        <SidebarGroup>
+                            <SidebarGroupLabel>Car Owner</SidebarGroupLabel>
 
-                    <SidebarGroupContent>
-                        <SidebarMenu>
-                            <SidebarMenuItem>
-                                <SidebarMenuButton className="rounded-2xl">
-                                    <NavLink
-                                        to="/adminhome"
-                                        className={({ isActive }) =>
-                                            `flex items-center gap-2 py-3 w-full h-full rounded-2xl ${isActive
-                                                ? "bg-accent text-accent-foreground"
-                                                : "text-muted-foreground"
-                                            }`
-                                        }
-                                    >
-                                        <Home className="w-4 h-4" />
-                                        <span>Dashboard</span>
-                                    </NavLink>
-                                </SidebarMenuButton>
-                            </SidebarMenuItem>
+                            <SidebarGroupContent>
+                                <SidebarMenu>
+                                    <SidebarMenuItem>
+                                        <SidebarMenuButton className="rounded-2xl">
+                                            <NavLink
+                                                to="/carownerhome"
+                                                className={({ isActive }) => linkActive(isActive)}
+                                            >
+                                                <Home className="w-4 h-4" />
+                                                <span>Dashboard</span>
+                                            </NavLink>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
 
-                            <SidebarMenuItem>
-                                <SidebarMenuButton className="rounded-2xl">
-                                    <NavLink
-                                        to="/manageusers"
-                                        className={({ isActive }) =>
-                                            `flex items-center gap-2 py-3 w-full h-full rounded-2xl ${isActive
-                                                ? "bg-accent text-accent-foreground"
-                                                : "text-muted-foreground"
-                                            }`
-                                        }
-                                    >
-                                        <Users className="w-4 h-4" />
-                                        <span>Manage Users</span>
-                                    </NavLink>
-                                </SidebarMenuButton>
-                            </SidebarMenuItem>
-                        </SidebarMenu>
-                    </SidebarGroupContent>
-                </SidebarGroup>
+                                    <SidebarMenuItem>
+                                        <SidebarMenuButton className="rounded-2xl">
+                                            <NavLink
+                                                to="/manageCar"
+                                                className={({ isActive }) => linkActive(isActive)}
+                                            >
+                                                <CarFront className="w-4 h-4" />
+                                                <span>Manage Cars</span>
+                                            </NavLink>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                                </SidebarMenu>
+                            </SidebarGroupContent>
+                        </SidebarGroup>
+                    </>}
+                {type === "admin" &&
+                    <>
+                        <SidebarGroup>
+                            <SidebarGroupLabel>Admin</SidebarGroupLabel>
+
+                            <SidebarGroupContent>
+                                <SidebarMenu>
+                                    <SidebarMenuItem>
+                                        <SidebarMenuButton className="rounded-2xl">
+                                            <NavLink
+                                                to="/adminhome"
+                                                className={({ isActive }) => linkActive(isActive)}
+                                            >
+                                                <Home className="w-4 h-4" />
+                                                <span>Dashboard</span>
+                                            </NavLink>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+
+                                    <SidebarMenuItem>
+                                        <SidebarMenuButton className="rounded-2xl">
+                                            <NavLink
+                                                to="/manageusers"
+                                                className={({ isActive }) => linkActive(isActive)}
+                                            >
+                                                <Users className="w-4 h-4" />
+                                                <span>Manage Users</span>
+                                            </NavLink>
+                                        </SidebarMenuButton>
+                                    </SidebarMenuItem>
+                                </SidebarMenu>
+                            </SidebarGroupContent>
+                        </SidebarGroup>
+                    </>}
             </SidebarContent>
 
             <SidebarRail />
