6c7eac8b57546e9c3e4c68a279a3387fd3bd89c3
Omar Nezar <frcollege2@gmail.com>
2026-07-23 12:26:02 +0400

    Added vin

--- server/models/vehicleModel.ts
+++ server/models/vehicleModel.ts
@@ -1,46 +1,51 @@
 import mongoose, { Schema, Document, Types, type HydratedDocument } from "mongoose";
 
 interface IVehicle {
-  owner: Types.ObjectId;
-  make: string;
-  model: string;
-  year: number;
-  plateNumber: string;
-  mileage: number;
-  createdAt: Date;
-  updatedAt: Date;
+    owner: Types.ObjectId;
+    make: string;
+    model: string;
+    year: number;
+    plateNumber: string;
+    mileage: number;
+    vin: string;
+    createdAt: Date;
+    updatedAt: Date;
 }
 
 const vehicleSchema = new Schema<IVehicle>(
-  {
-    owner: {
-      type: Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
+    {
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+        make: {
+            type: String,
+            required: true,
+        },
+        model: {
+            type: String,
+            required: true,
+        },
+        year: {
+            type: Number,
+            required: true,
+        },
+        plateNumber: {
+            type: String,
+            required: true,
+        },
+        mileage: {
+            type: Number,
+            required: true,
+        },
+        vin: {
+            type: String,
+            required: true,
+            unique: true
+        }
     },
-    make: {
-      type: String,
-      required: true,
-    },
-    model: {
-      type: String,
-      required: true,
-    },
-    year: {
-      type: Number,
-      required: true,
-    },
-    plateNumber: {
-      type: String,
-      required: true,
-      unique: true, // optional
-    },
-    mileage: {
-        type: Number,
-        required: true,
-    }
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 export type VehicleDocument = HydratedDocument<IVehicle>;
