cd336cc64e3d5971725669676e975cdddd9562c6
Omar Nezar <frcollege2@gmail.com>
2026-07-29 12:57:18 +0400

    Formatting

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -3,18 +3,18 @@ import { type Request, type Response } from "express";
 import userModel from "../models/userModel.js";
 
 export const getUsers = async (req: Request, res: Response) => {
-    try {
-        const users = await userModel.find();
-        return res.status(200).json({
-            message: "Users fetched successfully",
-            users,
-        });
-    } catch (error) {
-        console.error(error);
-        return res.status(500).json({
-            message: "Server error",
-        });
-    }
+  try {
+    const users = await userModel.find();
+    return res.status(200).json({
+      message: "Users fetched successfully",
+      users,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Server error",
+    });
+  }
 }
 
 export const deleteUser = async (req: Request, res: Response) => {
