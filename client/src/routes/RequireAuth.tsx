1bc063621e855e723934b7a59fb73fe24d491a5f
Omar Nezar <frcollege2@gmail.com>
2026-08-03 12:00:38 +0400

    Now reads token from redux to detect session expiry

--- client/src/routes/RequireAuth.tsx
+++ client/src/routes/RequireAuth.tsx
@@ -1,10 +1,15 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useAppSelector } from "../store/hooks";
 
 export default function RequireAuth() {
-    const token = localStorage.getItem("authToken")
+    const { token, msg } = useAppSelector((state) => state.auth) || { token: localStorage.getItem("authToken"), msg: null };
 
     if (!token) {
-        return <Navigate to="/login" replace state={{ msg: "Unauthorized Access!" }} />;
+        if (!msg) {
+            return <Navigate to="/login" replace state={{ msg: "Unauthorized Access!" }} />;
+        } else {
+            return <Navigate to="/login" replace state={{ msg, description: "Please log in again." }} />;
+        }
     }
 
     return <Outlet />;
